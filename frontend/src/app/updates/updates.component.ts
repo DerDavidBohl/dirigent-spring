aa1d20eb3d11ab6d7428df09a4ff0f1a7b8cfc6d
DerDavidBohl <david@bohl24.de>
2026-02-02 23:37:41 +0100

    feat: add progress spinner on update reload in frontend

--- frontend/src/app/updates/updates.component.ts
+++ frontend/src/app/updates/updates.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api/api.service';
-import { interval, Observable } from 'rxjs';
+import { interval, Observable, tap } from 'rxjs';
 import { DeploymentUpdate } from '../api/deployment-update';
 import { MatTable, MatColumnDef, MatHeaderRowDef, MatHeaderCellDef, MatCellDef, MatRowDef, MatTableModule } from "@angular/material/table";
 import { MatAnchor } from "@angular/material/button";
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-updates',
-  imports: [MatTableModule, MatTable, MatColumnDef, MatHeaderRowDef, MatHeaderCellDef, MatCellDef, MatAnchor, MatRowDef],
+  imports: [MatTableModule, MatTable, MatColumnDef, MatHeaderRowDef, MatHeaderCellDef, MatCellDef, MatAnchor, MatRowDef, MatProgressSpinner, AsyncPipe],
   templateUrl: './updates.component.html'
 })
 export class UpdatesComponent {
   updates$: Observable<Array<DeploymentUpdate>>;
+  updatesReloading$: Observable<boolean>;
   isCheckingForUpdates = false;
 
   constructor(private apiService: ApiService) {
     this.updates$ = apiService.deploymentUpdates$;
+    this.updatesReloading$ = apiService.deploymentUpdatesReloading$.pipe(tap(console.log));
+
     apiService.reloadDeployementUpdates();
 
     interval(2000).subscribe(() => {
