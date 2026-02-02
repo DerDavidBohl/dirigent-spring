5ec3fc2dbd7deb7c9274125ae72ef93d780a55aa
DerDavidBohl <david@bohl24.de>
2026-02-02 20:15:09 +0100

    fix: update deployments in smaller transactions

--- backend/src/main/java/org/davidbohl/dirigent/deployments/updates/DeploymentUpdateService.java
+++ backend/src/main/java/org/davidbohl/dirigent/deployments/updates/DeploymentUpdateService.java
@@ -58,7 +58,6 @@ public class DeploymentUpdateService {
     private String composeCommand;
     
     @Async
-    @Transactional
     public void updateDeployment(DeploymentUpdateDto deploymentUpdate) {
 
         List<DeploymentUpdateEntity> entities = markAsRunning(deploymentUpdate);
